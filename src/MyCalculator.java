06f7a648f92e8b8101801254586e9a9a6f1eca5a
SubZero <subzeropl@gmail.com>
2019-01-15 08:43:58 +0100

    - param checking - exception handling - input loop

--- src/MyCalculator.java
+++ src/MyCalculator.java
@@ -1,7 +1,34 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class MyCalculator {
     public static void main(String[] args) {
         MyCalculator c = new MyCalculator();
-        System.out.println(c.power(1, 1));
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            while (true) {
+                try {
+                    String line = br.readLine();
+                    if (line.isEmpty())
+                        break;
+                    StringTokenizer st = new StringTokenizer(line, " ");
+                    int a = Integer.parseInt(st.nextToken());
+                    int b = Integer.parseInt(st.nextToken());
+                    System.out.println(c.power(a, b));
+                } catch (NumberFormatException nfe) {
+                    System.out.println("Wrong input:");
+                    nfe.printStackTrace();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Input error");
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -10,8 +37,13 @@ public class MyCalculator {
      * @param n base
      * @param p exponent
      * @return n<sup>p</sup>
+     * @throws Exception when n is negative or p is negative or when n and p is zero
      */
-    private long power(int n, int p) {
+    private long power(int n, int p) throws Exception {
+        if (n < 0 || p < 0)
+            throw new Exception("n or p should not be negative");
+        if (n == 0 && p == 0)
+            throw new Exception("n and p should not be zero");
         return (long) Math.pow(n, p);
     }
 }
